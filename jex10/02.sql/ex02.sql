939844882d25688fe6ac7a8f73bcec91c3fde665
puchochiri <doawishfor@naver.com>
2022-11-15 13:34:21 +0900

    commit

--- jex10/02.sql/ex02.sql
+++ jex10/02.sql/ex02.sql
@@ -94,3 +94,74 @@ ALTER TABLE tbl_reply DROP CONSTRAINT FK_REPLY_BOARD;
 -- 제약조건 수정
 ALTER TABLE tbl_reply ADD CONSTRAINT FK_REPLY_BOARD FOREIGN KEY(bno) 
 REFERENCES tbl_board (bno) ON DELETE CASCADE;
+
+
+
+-- 스프링 시큐리티의 지정된 테이블을 생성하는 SQL
+
+create table users(
+    username varchar2(50) not null primary key,
+    password varchar2(50) not null,
+    enabled char(1) default '1'
+);
+
+
+create table authorities (
+    username varchar2(50) not null,
+    authority varchar2(50) not null,
+    constraint fk_authorities_users foreign key(username) references users(username)
+    
+);
+
+create unique index ix_auth_username on authorities (username, authority);
+
+insert into users (username, password) values ('user00', 'pw00');
+insert into users (username, password) values ('member00', 'pw00');
+insert into users (username, password) values ('admin00', 'pw00');
+
+insert into authorities (username, authority) values ('user00', 'ROLE_USER');
+insert into authorities (username, authority) values ('member00', 'ROLE_MANAGER');
+insert into authorities (username, authority) values ('admin00', 'ROLE_MANAGER');
+insert into authorities (username, authority) values ('admin00', 'ROLE_ADMIN');
+    
+commit;
+
+
+select * from users
+
+select * from authorities
+
+create table tbl_member(
+    userid varchar2(50) not null primary key,
+    userpw varchar2(100) not null,
+    username varchar2(100) not null,
+    regdate date default sysdate,
+    updatedate date default sysdate,
+    enabled char(1) default '1'
+);
+
+
+create table tbl_member_auth (
+    userid varchar2(50) not null,
+    auth    varchar2(50) not null,
+    constraint fk_member_auth foreign key(userid) references tbl_member(userid)
+);
+
+-- users by users -query
+select userid username, userpw password, enabled from tbl_member
+where userid ='admin90'
+
+
+
+-- authorities-by-username-query
+select userid username, auth authority from tbl_member_auth where userid = 'admin90'
+
+create table persistent_logins (
+ username varchar(64) not null,
+ series varchar(64) primary key,
+ token varchar(64) not null,
+ last_used timestamp not null
+);
+ 
+            
+select * from persistent_logins
